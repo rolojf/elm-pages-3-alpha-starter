4c93b15ffd510fb78e52079e7e66564892f95d46
Rolando Flores <rolo@tutamail.com>
2022-03-28 19:02:42 -0600

    helper functions para el menu

--- app/Shared.elm
+++ app/Shared.elm
@@ -124,8 +124,108 @@ view sharedData page model toMsg pageView =
     }
 
 
-viewMenu : Html msg
-viewMenu =
+viewMenu : List View.Liga -> Html msg
+viewMenu ligas =
+    let
+        ligasNormales =
+            List.filter (\liga -> liga.especial == False) ligas
+
+        ligasEspeciales =
+            List.filter .especial ligas
+
+        ligaNormalDesk : Html msg
+        ligaNormalDesk =
+            Html.nav
+                [ class "hidden md:flex space-x-10"
+                ]
+                (List.map
+                    (\liga ->
+                        case liga.dir of
+                            View.Otra camino ->
+                                Html.a
+                                    [ Attr.href <| Path.toRelative camino
+                                    , class "text-base font-medium text-gray-500 hover:text-gray-900"
+                                    ]
+                                    [ text liga.queDice ]
+
+                            View.Interna rutaLiga ->
+                                Route.link
+                                    rutaLiga
+                                    [ class "text-base font-medium text-gray-500 hover:text-gray-900" ]
+                                    [ text liga.queDice ]
+                    )
+                    ligasNormales
+                )
+
+        ligaEspecialDesk : Html msg
+        ligaEspecialDesk =
+            div
+                [ class "hidden md:flex items-center justify-end md:flex-1 lg:w-0" ]
+                (List.map
+                    (\liga ->
+                        case liga.dir of
+                            View.Otra camino ->
+                                Html.a
+                                    [ Attr.href <| Path.toRelative camino
+                                    , class "ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                    ]
+                                    [ text liga.queDice ]
+
+                            View.Interna rutaLiga ->
+                                Route.link
+                                    rutaLiga
+                                    [ class "ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" ]
+                                    [ text liga.queDice ]
+                    )
+                    ligasEspeciales
+                )
+
+        ligaNormalMovil =
+            List.map
+                (\liga ->
+                    case liga.dir of
+                        View.Otra camino ->
+                            Html.a
+                                [ Attr.href <| Path.toRelative camino
+                                , class "-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                                ]
+                                [ Html.span
+                                    [ class "ml-3 text-base font-medium text-gray-900" ]
+                                    [ text liga.queDice ]
+                                ]
+
+                        View.Interna rutaLiga ->
+                            Route.link
+                                rutaLiga
+                                [ class "-m-3 p-3 flex items-center rounded-md hover:bg-gray-50" ]
+                                [ Html.span
+                                    [ class "ml-3 text-base font-medium text-gray-900" ]
+                                    [ text liga.queDice ]
+                                ]
+                )
+                ligasNormales
+
+        ligaEspecialMovil =
+            div []
+                (List.map
+                    (\liga ->
+                        case liga.dir of
+                            View.Otra camino ->
+                                Html.a
+                                    [ Attr.href <| Path.toRelative camino
+                                    , class "w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                    ]
+                                    [ text liga.queDice ]
+
+                            View.Interna rutaLiga ->
+                                Route.link
+                                    rutaLiga
+                                    [ class "w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700" ]
+                                    [ text liga.queDice ]
+                    )
+                    ligasEspeciales
+                )
+    in
     div
         [ class "relative bg-white"
         ]
@@ -158,39 +258,8 @@ viewMenu =
                         , HeroIcons.outlineMenu
                         ]
                     ]
-                , Html.nav
-                    [ class "hidden md:flex space-x-10"
-                    ]
-                    [ Html.a
-                        [ Attr.href "#"
-                        , class "text-base font-medium text-gray-500 hover:text-gray-900"
-                        ]
-                        [ text "Solutions" ]
-                    , Html.a
-                        [ Attr.href "#"
-                        , class "text-base font-medium text-gray-500 hover:text-gray-900"
-                        ]
-                        [ text "Pricing" ]
-                    , Html.a
-                        [ Attr.href "#"
-                        , class "text-base font-medium text-gray-500 hover:text-gray-900"
-                        ]
-                        [ text "Docs" ]
-                    , Html.a
-                        [ Attr.href "#"
-                        , class "text-base font-medium text-gray-500 hover:text-gray-900"
-                        ]
-                        [ text "More" ]
-                    ]
-                , div
-                    [ class "hidden md:flex items-center justify-end md:flex-1 lg:w-0"
-                    ]
-                    [ Html.a
-                        [ Attr.href "#"
-                        , class "ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                        ]
-                        [ text "Sign up" ]
-                    ]
+                , ligaNormalDesk
+                , ligaEspecialDesk
                 ]
             ]
         , {-
@@ -239,51 +308,16 @@ viewMenu =
                             ]
                         ]
                     , div
-                        [ class "mt-6"
-                        ]
+                        [ class "mt-6" ]
                         [ Html.nav
-                            [ class "grid gap-y-8"
-                            ]
-                            [ Html.a
-                                [ Attr.href "#"
-                                , class "-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                                ]
-                                [ Html.span
-                                    [ class "ml-3 text-base font-medium text-gray-900"
-                                    ]
-                                    [ text "Analytics" ]
-                                ]
-                            , Html.a
-                                [ Attr.href "#"
-                                , class "-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                                ]
-                                [ Html.span
-                                    [ class "ml-3 text-base font-medium text-gray-900"
-                                    ]
-                                    [ text "Engagement" ]
-                                ]
-                            , Html.a
-                                [ Attr.href "#"
-                                , class "-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                                ]
-                                [ Html.span
-                                    [ class "ml-3 text-base font-medium text-gray-900"
-                                    ]
-                                    [ text "Security" ]
-                                ]
-                            ]
+                            [ class "grid gap-y-8" ]
+                            ligaNormalMovil
                         ]
                     ]
                 , div
                     [ class "py-6 px-5 space-y-6"
                     ]
-                    [ div []
-                        [ Html.a
-                            [ Attr.href "#"
-                            , class "w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                            ]
-                            [ text "Sign up" ]
-                        ]
+                    [ ligaEspecialMovil
                     ]
                 ]
             ]
