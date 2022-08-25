66e9675802bcd0fba548b1c3e41368527ae4bf7f
Rolando Flores <rolo@tutamail.com>
2022-08-24 19:42:47 -0500

    Agreguar favicons a Site.elm Head tag

--- app/Site.elm
+++ app/Site.elm
@@ -3,17 +3,23 @@ module Site exposing (config)
 import DataSource exposing (DataSource)
 import Head
 import SiteConfig exposing (SiteConfig)
+import MimeType
+import Pages.Url as Url
 
 
 config : SiteConfig
 config =
-    { canonicalUrl = "https://elm-pages.com"
+    { canonicalUrl = "https://solarpaq.com"
     , head = head
     }
 
 
 head : DataSource (List Head.Tag)
 head =
-    [ Head.sitemapLink "/sitemap.xml"
+    [ --Head.sitemapLink "/sitemap.xml"
+    Head.icon [(32, 32)] MimeType.Png (Url.external "/favicon.ico")
+    , Head.icon [ ] (MimeType.OtherImage "svg+xml") (Url.external "/icon.svg")
+    , Head.appleTouchIcon (Just 180) (Url.external "/apple-touch-icon.png")
+
     ]
         |> DataSource.succeed
